fee262de9fee5b4934d0a8a2c08927c90ab124ff
wyv <wyvernusx@gmail.com>
2026-08-20 23:45:11 -0400

    updated menu buttons to make them work, added final level

--- Assets/MenuButtons.cs
+++ Assets/MenuButtons.cs
@@ -11,12 +11,7 @@ public class MenuButtons : MonoBehaviour
       [2] = "LevelTwo",
       [3] = "LevelThree",
       [4] = "LevelFour",
-      [5] = "LevelFive",
-      [6] = "LevelSix",
-      [7] = "LevelSeven",
-      [8] = "LevelEight",
-      [9] = "LevelNine",
-      [10] = "LevelTen"
+      [5] = "LevelFive", 
     };
      
     void Start() {
--- Assets/ObjectRotate.cs
+++ Assets/ObjectRotate.cs
@@ -6,12 +6,38 @@ public class ObjectRotate : MonoBehaviour
     [Header("Object References")]
     public GameObject rotatedObj;
     public GameObject moveObj;
+    float max = -4.98f;
+    float min = 5.69f;
+    float time = 0.0f;
+    /*Vector3 objectStartPosition = new Vector3();
 
     void Start() {
-        
+      objectStartPosition = moveObj.gameObject.transform.position;  
     }
- 
+
+    void moveToPos(GameObject movingObject, Vector3 pos1, Vector3 pos2) {
+      if (movingObject.transform.position == pos1) {
+        Vector3.MoveTowards(objectStartPosition, new Vector3(4.9f, 1.44f, -47.66f), 5f);
+      } else if (movingObject.transform.position == pos2) {
+        Vector3.MoveTowards(new Vector3(4.9f, 1.44f, -47.66f), objectStartPosition, 5f);
+      } 
+    }*/
+
     void Update() {
-      rotatedObj.transform.Rotate(Vector3.up * 65f * Time.deltaTime);
+      rotatedObj.transform.Rotate(Vector3.up * 65f * Time.deltaTime);  
+      moveObj.transform.position = new Vector3(Mathf.Lerp(min, max, time), 1.44f, -47.66f); 
+      time += 0.5f * Time.deltaTime; 
+      
+      if (time > 1.0f) {
+        float temp = max;
+        max = min;
+        min = temp;
+        time = 0.0f;
+      }
+
+      //moveToPos(moveObj, objectStartPosition, new Vector3(4.9f, 1.44f, -47.66f)); 
+    
+      /*float xPos = Mathf.PingPong(Time.deltaTime * 2f, 5f * 2) - 5f;
+      transform.position = new Vector3(xPos, transform.position.y, transform.position.z);*/ 
     }
 }
